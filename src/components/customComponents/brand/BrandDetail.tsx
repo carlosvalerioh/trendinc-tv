959efecbaecc1c90fc1776d08ad95e55fabf6264
Alvaro castillo <6666alvaro666@gmail.com>
2021-08-06 19:11:49 -0500

    :tada: 1 review

--- src/components/customComponents/brand/BrandDetail.tsx
+++ src/components/customComponents/brand/BrandDetail.tsx
@@ -1,75 +1,83 @@
-import React from "react" 
+import React from "react"
 
-export const BrandDetail = ({children}) => {
-    const style = {
-        
-    }
-    return (
-        <div style={style} className="mt-16 mb-0 flex flex-col justify-between gap-5">
+export const BrandDetail = ({ children }) => {
+    const style = {}
+    return children === undefined ? (
+        " "
+    ) : (
+        <div
+            style={style}
+            className="mt-64 mb-0 flex flex-col justify-between gap-5"
+        >
             {children}
         </div>
     )
 }
 
-
-export const BrandDetailImage = ({ children }) =>{
-    const style = {
-        
-    }
-    return (
+export const BrandDetailImage = ({ children }) => {
+    const style = {}
+    return children === undefined ? (
+        " "
+    ) : (
         <div style={style} className="flex-1  items-end">
             {children}
         </div>
     )
 }
-export const BrandDetailTitle = ({ children }) =>{
+export const BrandDetailTitle = ({ children,color}) => {
     const style = {
-        
+        color: color
     }
-    return (
-        <div style={style} className="flex-1">
-            {children}
+    return children === undefined ? (
+        " "
+    ) : (
+        <div  className="flex-1">
+           <h2  style={style} className="text-2xl">{children}</h2>
         </div>
     )
 }
-export const BrandDetailDescription = ({ children }) =>{
-    const style = {
-        
-    }
-    return (
+export const BrandDetailDescription = ({ children }) => {
+    const style = {}
+    return children === undefined ? (
+        " "
+    ) : (
         <div style={style} className="px-5 max-w-100px">
-           <p>{children}</p>
+            <p>{children}</p>
         </div>
     )
 }
-export const BrandDetailHashtag = ({ children }) =>{
+export const BrandDetailHashtag = ({ children,color }) => {
     const style = {
-        
+        color:color
     }
-    return (
-        <div style={style} className="flex-1 items-end text-right">
-            {children}
-        </div>
-    )
-}
-export const BrandDetailHeader = ({ children }) =>{
-    const style = {
-        
-    }
-    return (
-        <div style={style} className="flex">
-            {children}
-        </div>
-    )
-}
-export const BrandDetailText = ({ children }) =>{
-    const style = {
-        
-    }
-    return (
-        <div style={style} className="">
-            {children}
-        </div>
-    )
-}
 
+    return children === undefined ? (
+        " "
+    ) : (
+        <div  className="flex-1 items-end text-right">
+            <div style={style} className="font-bold"><p>{children}</p></div>
+        </div>
+    )
+}
+export const BrandDetailHeader = ({ children }) => {
+    const style = {}
+    return children === undefined ? (
+        " "
+    ) : (
+        <div style={style} className="flex items-center">
+            {children}
+        </div>
+    )
+}
+export const BrandDetailText = ({ children }) => {
+    const style = {
+       
+    }
+    return children === undefined ? (
+        " "
+    ) : (
+        <div style={style} className="pr-5 flex-end ">
+            <p>{children}</p>
+        </div>
+    )
+}
--- src/components/customComponents/videos/Videos.tsx
+++ src/components/customComponents/videos/Videos.tsx
@@ -1,21 +1,20 @@
 import React from "react"
 
 export const Videos = ({ children }) => {
-    const style = {
-        
-    }
+    const style = {}
 
     return (
-        <div style={style} className="my-7 grid grid-cols-1 sm:p-0 gap-4  md:grid-cols-2  md:p-4 2xl:gap-8 ">
+        <div
+            style={style}
+            className="my-7 grid grid-cols-1 auto-rows-auto  sm:p-0 gap-4  md:grid-cols-2   2xl:gap-8 grid-flow-row "
+        >
             {children}
         </div>
     )
 }
 
 export const Video = ({ video }) => {
-    const style = {
-       
-    }
+    const style = {}
 
     return (
         <div style={style} className="">
@@ -23,8 +22,24 @@ export const Video = ({ video }) => {
                 title="vimeo-player"
                 src={video}
                 width="100%"
-                height="270"
+                height="270px"
             ></iframe>
+        </div>
+    )
+}
+
+
+export const UniqueVideo = ({ children }) => {
+    const style = {
+        border:""
+    }
+
+    return (
+        <div
+            style={style}
+            className=""
+        >
+            {children}
         </div>
     )
 }
